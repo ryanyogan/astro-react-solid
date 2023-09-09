3cb68f5285266a2eef0de171d62e56e818782d56
Ryan Yogan <ryanyogan@gmail.com>
2023-09-09 08:48:03 -0500

    feat // run node not deno

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,5 @@ import netlify from '@astrojs/netlify/functions';
 export default defineConfig({
 	integrations: [react(), solidJs()],
 	output: 'server',
-	adapter: netlify({
-		edgeMiddleware: true,
-	}),
+	adapter: netlify(),
 });
