986033889a8a9c27f59bdaefd302dc9602def237
Ryan Yogan <ryanyogan@gmail.com>
2023-09-10 16:29:01 -0500

    feat // switch to vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import react from '@astrojs/react';
 import solidJs from '@astrojs/solid-js';
 import { defineConfig } from 'astro/config';
 
-import netlify from "@astrojs/netlify/functions";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
@@ -13,5 +13,5 @@ export default defineConfig({
     include: ['**/solid/*']
   })],
   output: 'hybrid',
-  adapter: netlify()
+  adapter: vercel()
 });
