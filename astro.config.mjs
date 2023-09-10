c963e4a85fe663710b9e65281f7a0f5b1e59bd64
Ryan Yogan <ryanyogan@gmail.com>
2023-09-09 20:23:02 -0500

    feat // netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,16 @@ import react from '@astrojs/react';
 import solidJs from '@astrojs/solid-js';
 import { defineConfig } from 'astro/config';
 
+import netlify from "@astrojs/netlify/functions";
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [
-		react({
-			include: ['**/react/*'],
-		}),
-		solidJs({
-			include: ['**/solid/*'],
-		}),
-	],
-	output: 'hybrid',
+  site: 'https://example.com',
+  integrations: [react({
+    include: ['**/react/*']
+  }), solidJs({
+    include: ['**/solid/*']
+  })],
+  output: 'hybrid',
+  adapter: netlify()
 });
